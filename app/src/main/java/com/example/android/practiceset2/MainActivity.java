6414e175086c41ebd3b981db82a166ef257050a5
Chanda <36143292+ChandaB@users.noreply.github.com>
2018-10-26 20:47:55 -0400

    updated description
    Identifying the this is basically an empty activity

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //basic activity that sets the layout to the activity_main layout  Nothing else is happening at this point...
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
